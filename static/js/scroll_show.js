302168a08b1a1b068d5f55e568782765db49cebc
misya11p <miya33ps@gmail.com>
2022-07-08 13:52:10 +0900

    add animation

--- static/js/scroll_show.js
+++ static/js/scroll_show.js
@@ -1,2 +1,13 @@
-var mainBlocks = window.document.getElementsByClassName("main-block")
-console.log(mainBlocks.lenght)
+let triggerMargin = 500
+
+var mainBlocks = window.document.getElementsByClassName("main-block");
+var fadeIn = function() {
+    for (var block of mainBlocks) {
+        if (block.getBoundingClientRect().top < triggerMargin) {
+            block.classList.add('show');
+        }
+    }
+};
+
+window.addEventListener('load', fadeIn)
+window.addEventListener('scroll', fadeIn)
